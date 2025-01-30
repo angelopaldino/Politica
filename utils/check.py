a6bd82c280faefe7cc49ef895d846660f3181363
angelopaldino <paldinoangelo01@gmail.com>
2025-01-30 15:28:47 +0100

    -fix bugs

--- utils/check.py
+++
@@ -1,6 +0,0 @@
-from pyspark import SparkContext
-sc = SparkContext.getOrCreate()
-sc.addPyFile("path_to_vaderSentiment.zip")
-
-from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
-analyzer = SentimentIntensityAnalyzer()
